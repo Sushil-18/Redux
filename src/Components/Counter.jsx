c1c3a09bf085c306a1f8540be31d43106f8e8c13
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-05-07 12:55:15 +0530

    Completed first tutorial on redux

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "../UI/Button";
+import { useSelector, useDispatch } from "react-redux";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const incrementHandler = () => {
+    dispatch({
+      type: "increment",
+    });
+  };
+
+  const decrementHandler = () => {
+    dispatch({
+      type: "decrement",
+    });
+  };
+  return (
+    <div className="bg-white flex flex-col gap-3 items-center justify-center w-[60%] mx-auto border-transperant rounded-xl mt-16 py-4">
+      <p className="text-black text-xl font-semibold"> Redux Counter</p>
+      <p className="text-black">{counter}</p>
+      <div className="flex flex-row gap-4">
+        <Button buttonName="Increment" onClick={incrementHandler} />
+        <Button buttonName="Decrement" onClick={decrementHandler} />
+      </div>
+      <Button buttonName="Toggle Counter" />
+    </div>
+  );
+};
+
+export default Counter;
